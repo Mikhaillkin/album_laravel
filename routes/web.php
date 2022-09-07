25d8137a08b970e488f7dd9da83695638da347cb
mikhaillkin <mikhaillkin@gmail.com>
2022-09-07 14:20:14 +0300

    Initial commit.Created 2 routes,Albums/AlbumsController,Main/MainController,and views for created routes

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Http\Controllers\Main\MainController;
+use App\Http\Controllers\Albums\AlbumsController;
+use Illuminate\Support\Facades\Route;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [MainController::class,'index'])->name('main.index');
+
+
+Route::group(['namespace' => 'Album','prefix'=>'albums'], function() {
+    Route::get('/', [AlbumsController::class,'index'])->name('album.index');
+//    Route::get('/{album}', [AlbumsController::class,'show'])->name('album.show');
+    Route::get('/add_album', [AlbumsController::class,'create'])->name('album.create');
+});
+
+
+//Auth::routes();
+
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
