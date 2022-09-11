566e5c89ca3d1a9ce608b41ce03f551460e8e8bc
mikhaillkin <mikhaillkin@gmail.com>
2022-09-11 11:18:01 +0300

    Made layout for login and register pages

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -71,48 +71,37 @@
 {{--        </form>--}}
 {{--    </x-auth-card>--}}
 {{--</x-guest-layout>--}}
-        <!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
 
-    <title>Signin Template for Bootstrap</title>
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/sign-in/">
+@extends('layouts.login')
 
-    <!-- Bootstrap core CSS -->
-    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+@section('content')
+    <body class="text-center">
+    <form action="{{ route('register') }}" method="POST" class="form-signin">
+        @csrf
+        <img class="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72"
+             height="72">
+        <h1 class="h3 mb-3 font-weight-normal">Please sign up</h1>
+        <label for="name" class="sr-only">Name</label>
+        <input type="text" id="name" name="name" class="form-control" placeholder="Name" required autofocus>
+        <label for="phone_number" class="sr-only">Phone number</label>
+        <input type="text" id="phone_number" name="phone_number" class="form-control" placeholder="+7ХХХХХХХХХХ"
+               required autofocus>
+        <label for="email" class="sr-only">Email address</label>
+        <input type="email" id="email" name="email" class="form-control" placeholder="Email address" required autofocus>
+        <label for="password" class="sr-only">Password</label>
+        <input type="password" id="password" style="margin-bottom: 0;" name="password" class="form-control"
+               placeholder="Password" required>
+        <label for="password_confirmation" class="sr-only">Confirm Password</label>
+        <input type="password" id="password_confirmation" name="password_confirmation" class="form-control"
+               placeholder="Confirm Password" required>
 
-    <!-- Custom styles for this template -->
-    <link href="css/login/signin.css" rel="stylesheet">
-</head>
-
-<body class="text-center">
-<form action="{{ route('register') }}" method="POST" class="form-signin">
-    @csrf
-    <img class="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-    <h1 class="h3 mb-3 font-weight-normal">Please sign up</h1>
-    <label for="name" class="sr-only">Name</label>
-    <input type="text" id="name" name="name" class="form-control" placeholder="Name" required autofocus>
-    <label for="phone_number" class="sr-only">Phone number</label>
-    <input type="text" id="phone_number" name="phone_number" class="form-control"  placeholder="+7ХХХХХХХХХХ" required autofocus>
-    <label for="email" class="sr-only">Email address</label>
-    <input type="email" id="email" name="email" class="form-control" placeholder="Email address" required autofocus>
-    <label for="password" class="sr-only">Password</label>
-    <input type="password" id="password" style="margin-bottom: 0;" name="password" class="form-control" placeholder="Password" required>
-    <label for="password_confirmation" class="sr-only">Confirm Password</label>
-    <input type="password" id="password_confirmation" name="password_confirmation" class="form-control" placeholder="Confirm Password" required>
-
-    <x-button class="btn btn-lg btn-primary btn-block" type="submit">
-        {{ __('Register') }}
-    </x-button>
-    <strong>Or</strong>
-    <a href="{{ route('login') }}" class="btn btn-lg btn-primary btn-block" >Login</a>
-    <p class="mt-5 mb-3 text-muted">&copy; 2017-2022</p>
-</form>
-</body>
-</html>
+        <x-button class="btn btn-lg btn-primary btn-block" type="submit">
+            {{ __('Register') }}
+        </x-button>
+        <strong>Or</strong>
+        <a href="{{ route('login') }}" class="btn btn-lg btn-primary btn-block">Login</a>
+        <p class="mt-5 mb-3 text-muted">&copy; 2017-2022</p>
+    </form>
+    </body>
+@endsection
