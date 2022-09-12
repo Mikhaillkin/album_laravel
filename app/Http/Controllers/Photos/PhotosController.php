340cf7f2299f23ede49e9dfb12e3075e0511a8e4
mikhaillkin <mikhaillkin@gmail.com>
2022-09-12 11:36:39 +0300

    Pagination albums with orderBy last updated

--- app/Http/Controllers/Photos/PhotosController.php
+++ app/Http/Controllers/Photos/PhotosController.php
@@ -75,6 +75,7 @@ class PhotosController extends Controller
 
         $file = $request->file('image');
         $image = Storage::disk('public')->put('/photos', $file);
+        $album = Album::find($request->album_id);
 
         if($image){
             Photo::create([
@@ -83,6 +84,8 @@ class PhotosController extends Controller
                 'image' => Storage::url($image),
                 'album_id' => $request->album_id,
             ]);
+
+            $album->update([ 'updated_at' => now() ]);
 
             return response()->json(['code'=>1,'msg'=>'New image has been saved successfully']);
         };
